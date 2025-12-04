821e2c82d816cd3bbb10fe9f28ab733c34316c77
ge79zol <mikepan2289@gmail.com>
2025-12-04 21:21:30 +0100

    Change of project structure and added .gitignore

--- frontend/app.js
+++
@@ -1,8 +0,0 @@
-async function getPrice() {
-    const setId = document.getElementById("setId").value;
-    const response = await fetch(`http://127.0.0.1:5000/api/price/${setId}`);
-    const data = await response.json();
-
-    document.getElementById("result").innerText =
-        `Set ${data.set_id} price: ${data.price}`;
-}
